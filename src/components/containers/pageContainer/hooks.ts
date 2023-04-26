55ac5442493b94c16fb739527e46e4cd3a8dd617
unkly <u7gyuuu7gi@gmail.com>
2023-04-27 00:02:11 +0900

    fix:api logic

--- src/components/containers/pageContainer/hooks.ts
+++ src/components/containers/pageContainer/hooks.ts
@@ -7,13 +7,7 @@ export function useInjection() {
     const response = await axios
       .get(url, {
         headers: {
-          'User-Agent':
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
-          'Accept-Language': 'ja,en-US;q=0.9,en;q=0.8',
-          'Accept-Charset': 'application/x-www-form-urlencoded; charset=UTF-8',
-          Origin: 'https://developer.riotgames.com',
           'X-Riot-Token': 'RGAPI-9fa0f31d-0253-4927-86ae-096298016563',
-          'Content-Type': 'application/json;charset=utf-8',
         },
       })
       .then((res) => {
