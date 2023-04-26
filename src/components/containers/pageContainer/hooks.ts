91fc1a659a9c6ad72fc9d1738dd4fa541b72faa5
unkly <u7gyuuu7gi@gmail.com>
2023-04-27 00:15:35 +0900

    fix:api logic

--- src/components/containers/pageContainer/hooks.ts
+++ src/components/containers/pageContainer/hooks.ts
@@ -4,18 +4,14 @@ import useSWR from 'swr'
 export function useInjection() {
   const fetcher = async (url: string) => {
     console.log(url)
-    const response = await axios
-      .get(url, {
-        headers: {},
-      })
-      .then((res) => {
-        res.data()
-      })
+    const response = await axios.get(url, {}).then((res) => {
+      res.data()
+    })
     return response
   }
   const { data, error, isLoading } = useSWR(
     [
-      'https://asia.api.riotgames.com/riot/account/v1/accounts/by-riot-id/u7gi/JP1',
+      'https://asia.api.riotgames.com/riot/account/v1/accounts/by-riot-id/u7gi/JP1?api_key=RGAPI-9fa0f31d-0253-4927-86ae-096298016563',
     ],
     fetcher
   )
